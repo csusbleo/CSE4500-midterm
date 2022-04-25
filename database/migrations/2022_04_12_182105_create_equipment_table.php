f84328d6266538d924fea87a9e66ec52f7ce7586
Leo Li <98563019+csusbleo@users.noreply.github.com>
2022-04-25 16:54:33 -0700

    Update 2022_04_12_182105_create_equipment_table.php

--- database/migrations/2022_04_12_182105_create_equipment_table.php
+++ database/migrations/2022_04_12_182105_create_equipment_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->id();
             $table->foreignId('manufacture_id')->constrained();
             $table->foreignId('category_id')->constrained();
+            $table->foreignId('user_id')->constrained();
             $table->string('model');
             $table->string('CPU')->nullable();
             $table->string('memory')->nullable();
